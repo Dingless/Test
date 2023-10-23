2b0e6ec1b68db90893f0a8b73ea736e251f91dfb
Dingless <mohameddeg.abdulahi@elev.ga.ntig.se>
2023-10-23 10:28:45 +0200

    set1-3

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+class program 
+{
+  static void Main()
+  {
+    if (6 >= 3);
+    {
+        Console.Writeline("Hello World");
+    }
+  }
+
+}
